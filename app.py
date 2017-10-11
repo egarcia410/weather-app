568279ededc55932121e45a4cf3af6d25ebf7312
egarcia410 <egarcia410@gmail.com>
2017-10-10 23:47:41 -0500

    Update: added next 3 day forecast

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ import tornado.web
 import tornado.log
 import requests
 import urllib
+import datetime
 
 from dotenv import load_dotenv
 from jinja2 import Environment, PackageLoader, select_autoescape
 
 load_dotenv('.env')
 
-PORT = int(os.environ.get('PORT', '8000'))
+PORT = int(os.environ.get('PORT', '8080'))
 
 ENV = Environment(
     loader=PackageLoader('weather', 'templates'),
@@ -32,7 +33,6 @@ class MainHandler(TemplateHandler):
         # Request to Google Maps
         payload = {'address': encodeLocation, 'key': os.environ['KEY']}
         response = requests.get('https://maps.googleapis.com/maps/api/geocode/json', params=payload)
-        print(response.url)
         data = response.json()
         location = data['results'][0]['formatted_address']
         # Retrieve lat and long from Google Maps 
@@ -44,11 +44,20 @@ class MainHandler(TemplateHandler):
         # lng = '-95.1797696'
         # Request to DarkSky 
         response = requests.get('https://api.darksky.net/forecast/{}/{},{}'.format(os.environ['SECRET_KEY'], lat, lng))
+        print(response.url)
         data = response.json()
+        daily = {}
+        for info in range(1, 4):
+            daily[str(info)] = {}
+            daily[str(info)]['time'] = datetime.datetime.fromtimestamp(data['daily']['data'][info]['time']).strftime('%a')
+            daily[str(info)]['summary'] = data['daily']['data'][info]['summary']
+            daily[str(info)]['icon'] = data['daily']['data'][info]['icon']
+            daily[str(info)]["temperatureHigh"] = data['daily']['data'][info]["temperatureHigh"]
+            daily[str(info)]["temperatureLow"] = data['daily']['data'][info]["temperatureLow"]
         summary = data['currently']['summary']
         icon = data['currently']['icon']
         temp = data['currently']['temperature']
-        return self.render_template("home.html", {'location': location, 'summary': summary, 'icon': icon, 'temp': temp})
+        return self.render_template("home.html", {'location': location, 'summary': summary, 'icon': icon, 'temp': temp, 'daily': daily})
 
 def make_app():
     return tornado.web.Application([
